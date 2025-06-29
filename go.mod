c89091c5c1cff9d9e0bec4397609b6037ef59398
Gabriel Gomes <gabrielgomesdeveloper1999@gmail.com>
2025-06-29 21:10:15 +0100

    basic setup, get user input, validate input

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gabrielintech1999/pokedex-cli
+
+go 1.24.4
--- main.go
+++ main.go
@@ -1,12 +1,41 @@
 package main
 
-
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 
 
 func main() {
 
-  fmt.Println("Hello World!")
+  scanner := bufio.NewScanner(os.Stdin)
+  fmt.Println("Basic Pokedex-cli [press exit to terminate]")
+  fmt.Print(">>> ")
+
+  scanner.Scan()
+
+  text := scanner.Text()
+
+  cleanedInput := clearInput(text)
+   
+  fmt.Println("Echoing: ", cleanedInput)
   
 }
+
+
+
+func clearInput(str string) []string {
+  str = strings.TrimSpace(str)
+  str = strings.ToLower(str)
+  words := strings.Fields(str)
+
+  return words
+}
+
+
+// get user input
+// validate input
+// make a REPL
